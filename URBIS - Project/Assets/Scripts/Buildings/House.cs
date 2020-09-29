4a73b514a9c49501e5787b0a4ec80be472a97ad6
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-09-29 13:47:23 -0300

    Added tax income

---
+++ URBIS - Project/Assets/Scripts/Buildings/House.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : BaseBuilding
+{
+    [Tooltip("Tax income of this house per second.")]
+    [SerializeField] private float taxIncome = 1.0f;
+
+    public float TaxIncome => taxIncome;
+
+    private void Start()
+    {
+        if (CityManager.Instance)
+        {
+            CityManager.Instance.TrackHouse(this);
+        }
+        else
+        {
+            Debug.LogError("CityManager instance not set!");
+        }
+    }
+}
---
+++ URBIS - Project/Assets/Scripts/Managers/CityManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityManager : MonoBehaviour
+{
+    private static CityManager instance;
+
+    public static CityManager Instance => instance;
+
+    [Tooltip("Mark the object as DontDestroyOnLoad.")]
+    [SerializeField] private bool isPersistent;
+
+    [SerializeField] private HashSet<House> houses = new HashSet<House>();
+
+    [SerializeField] private float currentMoney;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            if(isPersistent)
+                DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        foreach (House house in houses)
+        {
+            currentMoney += house.TaxIncome * Time.deltaTime;
+        }
+    }
+
+    public void TrackHouse(House house)
+    {
+        houses.Add(house);
+    }
+}
