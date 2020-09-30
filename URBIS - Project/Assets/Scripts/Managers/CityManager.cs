b325b98223f208107803f6b9cde639ca9037d1ee
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-09-30 15:07:24 -0300

    Added random house spawn

--- URBIS - Project/Assets/Scripts/Managers/CityManager.cs
+++ URBIS - Project/Assets/Scripts/Managers/CityManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CityManager : MonoBehaviour
 {
@@ -15,16 +17,19 @@ public class CityManager : MonoBehaviour
     [SerializeField] private float startingMoney = 500;
     [Tooltip("Distance between houses, to be used when spawning houses so they stay in a grid-like pattern.")]
     [SerializeField] private Vector3 distanceBetweenHouses = new Vector3(10, 0, 10);
+    [Tooltip("Time between spawning a random house.")]
+    [SerializeField] private float spawnHouseCooldown = 5.0f;
     
     [Space]
     
     [Tooltip("Object that will be spawned at empty tiles.")]
     [SerializeField] private GameObject emptyTileHighlight;
 
+    [SerializeField] private GameObject housePrefab;
     [SerializeField] private GameObject hospitalPrefab;
     [SerializeField] private GameObject policeDepartmentPrefab;
     [SerializeField] private GameObject busStopPrefab;
-    
+
     [Space]
     
     [SerializeField] private TextMeshProUGUI guiCurrentMoney;
@@ -40,9 +45,12 @@ public class CityManager : MonoBehaviour
     [SerializeField] private float averageHappiness;
     [SerializeField] private float totalCarbonEmission;
     [SerializeField] private BuildingHouseState currentBuildingState = BuildingHouseState.NotBuilding;
-    
+    [SerializeField] private float currentHouseSpawnCooldown = 0.0f;
+
     private HashSet<House> houses = new HashSet<House>();
     private HashSet<EmptyTile> emptyTiles = new HashSet<EmptyTile>();
+
+    private EmptyTile[] emptyTilesArray;
 
     public Vector3 DistanceBetweenHouses
     {
@@ -74,7 +82,6 @@ public class CityManager : MonoBehaviour
         {
             return;
         }
-        //Debug.Log(hitobject.collider.gameObject, hitobject.collider.gameObject);
         if (hitobject.transform.TryGetComponent(out EmptyTile emptyTile))
         {
             if (currentBuildingState != BuildingHouseState.NotBuilding)
@@ -112,6 +119,24 @@ public class CityManager : MonoBehaviour
         guiAverageHappiness.SetText(averageHappiness.ToString("0.0"));
         guiAverageCarbonEmission.SetText(averageCarbonEmission.ToString("0.0"));
         guiTaxIncomePerSecond.SetText(averageCarbonEmission.ToString("0.00"));
+
+        currentHouseSpawnCooldown += Time.deltaTime;
+        if (currentHouseSpawnCooldown >= spawnHouseCooldown)
+        {
+            SpawnRandomHouse();
+            currentHouseSpawnCooldown = 0.0f;
+        }
+    }
+
+    private void SpawnRandomHouse()
+    {
+        var newHouse = Instantiate(housePrefab, transform.position, Quaternion.identity);
+        emptyTilesArray = emptyTiles.ToArray();
+        var randomTile = emptyTilesArray[Random.Range(0, emptyTilesArray.Length)];
+        newHouse.transform.position = randomTile.transform.position;
+
+        emptyTiles.Remove(randomTile);
+        Destroy(randomTile.gameObject);
     }
 
     public void TrackHouse(House house)
@@ -123,7 +148,11 @@ public class CityManager : MonoBehaviour
     {
         GameObject spawnedTile = Instantiate(emptyTileHighlight, tile, Quaternion.identity);
         spawnedTile.transform.SetParent(transform);
-        spawnedTile.SetActive(false);
+        if (currentBuildingState == BuildingHouseState.NotBuilding)
+        {
+            spawnedTile.SetActive(false);
+        }
+
         emptyTiles.Add(spawnedTile.GetComponentInChildren<EmptyTile>());
     }
 
